fc33ccb3e87a35e2eb4be728e4f4019cd9ddaf1b
Kurtmj93 <kurtmj93@gmail.com>
2023-02-18 17:24:38 -0500

    fix errors after changing queryURL variable to accommodate second fetch URL for forecast

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const weatherKey = '&APPID=365d89ed8b1f3231f3f36c6c09d489d6';
+const apiKey = '&APPID=365d89ed8b1f3231f3f36c6c09d489d6';
 const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
 
@@ -9,7 +9,7 @@ async function sendRequest(event) {
     event.preventDefault();
     let searchRequest = $('#query').val();
     let encodeSearch = encodeURI(searchRequest).trim();
-    let weatherQuery = queryURL + encodeSearch + weatherKey;
+    let weatherQuery = weatherURL + encodeSearch + apiKey;
     return fetch(weatherQuery, { method: 'GET' })
     .then(function(response) {
         return response.json();
@@ -21,7 +21,7 @@ async function sendRequest(event) {
         <div class="columns is one-fifth">
             <div class="card">
                 <div class="card-header">
-                <p class="card-header-title">${data.name} (${data.}</p>
+                <p class="card-header-title">${data.name}</p>
                 </div>
                 <div class="card-content">
                 <p></p>

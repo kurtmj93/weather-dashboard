3b48901be6a37efde73117c999b5f943f6c8c87e
Kurtmj93 <kurtmj93@gmail.com>
2023-02-18 17:20:40 -0500

    add bulma framework for front-end, implement search bar, implement initial weather data query fetch

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-const weatherKey = '365d89ed8b1f3231f3f36c6c09d489d6';
-let city;
-city = 'London,uk';
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + weatherKey;
-console.log(fetch(queryURL));
+const weatherKey = '&APPID=365d89ed8b1f3231f3f36c6c09d489d6';
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+
+let resultsBox = $('#results');
+
+
+async function sendRequest(event) {
+    event.preventDefault();
+    let searchRequest = $('#query').val();
+    let encodeSearch = encodeURI(searchRequest).trim();
+    let weatherQuery = queryURL + encodeSearch + weatherKey;
+    return fetch(weatherQuery, { method: 'GET' })
+    .then(function(response) {
+        return response.json();
+    })
+    .then (function(data) {
+        console.log(data);
+        let fahrenheit = Math.round((data.main.temp - 273.15) * (9/5) + 32);
+        resultsBox.append(`
+        <div class="columns is one-fifth">
+            <div class="card">
+                <div class="card-header">
+                <p class="card-header-title">${data.name} (${data.}</p>
+                </div>
+                <div class="card-content">
+                <p></p>
+                </div>
+                <div class="card-footer">
+                <p class="card-footer-item">${fahrenheit}°</p>
+                <p class="card-footer-item">Wind: ${data.wind.speed} MPH</p>
+                <p class="card-footer-item">Humidity: ${data.main.humidity}%</p>
+                </div>
+            </div>
+        </div>
+        `)
+    });
+}
+
+$('#search').submit(sendRequest);
